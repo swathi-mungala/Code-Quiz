03cccac26f8fba7785e80debfdc87f0259a85f22
Swathi <mswathi584@gmail.com>
2024-01-16 23:33:43 +0000

    Updated questions file with real questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,22 +1,27 @@
 var questions = [
     {
-        question : "Question 1",
-        options : ["option1","option2","option3","option4"],
-        answer : "option3"
+        question : "How do you write 'Hello World' in an alert box?",
+        options : ["msgBox('Hello World');","alertBox('Hello World');","alert('Hello World');","msg('Hello World');"],
+        answer : "alert('Hello World');"
     },
     {
-        question : "Question 2",
-        options : ["option1","option2","option3","option4"],
-        answer : "option1"
+        question : "How do you round the number 7.25, to the nearest integer?",
+        options : ["Math.rnd(7.25)","rnd(7.25)","round(7.25)","Math.round(7.25)"],
+        answer : "Math.round(7.25)"
     },
     {
-        question : "Question 3",
-        options : ["option1","option2","option3","option4"],
-        answer : "option4"
+        question : "How to write an IF statement in JavaScript?",
+        options : ["if i = 5 then","if (i == 5)","if i = 5","if i == 5 then"],
+        answer : "if (i == 5)"
     },
     {
-        question : "Question 4",
-        options : ["option1","option2","option3","option4"],
-        answer : "option1"
+        question : "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        options : ["if (i != 5)","if (i <> 5)","if i <> 5","if i =! 5 then"],
+        answer : "if (i != 5)"
+    },
+    {
+        question : "How does a FOR loop start?",
+        options : ["for (i <= 5; i++)","for (i = 0; i <= 5)","for (i = 0; i <= 5; i++)","for i = 1 to 5"],
+        answer : "for (i = 0; i <= 5; i++)"
     }
 ]
